b12a63f4c1bfeb4a724090d19a80ed7d5f836d7f
PranavReddy7 <pranavreddy7022@gmail.com>
2016-05-05 17:44:14 +0530

    Update mapJS.js

--- js/mapJS.js
+++ js/mapJS.js
@@ -5,39 +5,53 @@
   var directionsService ="";
   var directionsDisplay ="";
 
-/* instance variable for reference place of origin; */
+/* instance variable for referencing place of origin; */
   var r_loc ;
 
   /* main callback function */
 
   function initLocate(){
 
-    if(navigator.geolocation){
-      /* check wheteher browser supports geolocation feature or not
-         and if it is then load browser with user's current location using html geolocation feature.
-       */
-       navigator.geolocation.getCurrentPosition(function(p){
+    /* prompt user a confirmation that he wants to share his/her location or not*/
+    var confirmation = confirm("Press OK to share your location.");
 
-         var LatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
-         r_loc =LatLng;
-         initMap(LatLng);
-       });
+    if(confirmation == true){
+
+        /* if user shares his location than check browser supports geolocation feature or not */
+        if(navigator.geolocation){
+          /* check wheteher browser supports geolocation feature or not
+             and if it is then load browser with user's current location using html geolocation feature.
+           */
+           navigator.geolocation.getCurrentPosition(function(p){
+
+             var LatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
+             r_loc =LatLng;
+             initMap(LatLng);
+           });
+        }
+        else{
+            alert("geolocation is not supported in this browser.");
+
+            /* set default location */
+            var default_loc = {lat: 37.773972, lng: -122.431297};
+
+            /* set place of origin to default location */
+            r_loc = default_loc;
+            initMap(default_loc);
+        }
     }
     else{
-        alert("geolocation is not supported in this browser.");
+      /* otherwise set default location */
+      var default_loc = {lat: 37.773972, lng: -122.431297};
 
-        /* set default location */
-        var default_loc = {lat: 37.773972, lng: -122.431297};
-
-        /* set place of origin to default location */
-        r_loc =default_loc;
-        initMap(default_loc);
+      /* set place of origin to default location */
+      r_loc =default_loc;
+      initMap(default_loc);
     }
   }
 
 
   /* function to initialize google map */
-
   function initMap(current_loc) {
 
       directionsService = new google.maps.DirectionsService;
@@ -51,10 +65,10 @@
       var service = new google.maps.places.PlacesService(map);
       infowindow = new google.maps.InfoWindow();
       service.nearbySearch({
-              location: current_loc,
-              radius: 1500,    // Here the radius is set to 1500 so that all the gas stations within this radius will show up on the map.
-              type: ['gas_station']
-      }, processResults);     // callback function to process response
+            location: current_loc,
+            radius: 1500,         // Here the radius is set to 1500 so that all the gas stations within this radius will show up on the map.
+            type: ['gas_station']
+      }, processResults);         // callback function to process response
 
 
       var input = document.getElementById('pac-input');
@@ -129,29 +143,30 @@
 
       for (var i = 0, place; place = places[i]; i++) {
 
-    	last_places_count=i;
-        var image = {
-             url: place.icon,
-             size: new google.maps.Size(71, 71),
-             origin: new google.maps.Point(0, 0),
-             anchor: new google.maps.Point(17, 34),
-             scaledSize: new google.maps.Size(25, 25)
-        };
+    	  last_places_count=i;
+          var image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+          };
 
           /* Here im creating a marker
              so that wherever there is a gas station in that set radius
              a marker with the above charectaristics would pop up. */
 
-        marker[i] = new google.maps.Marker({
+          marker[i] = new google.maps.Marker({
                 map: map,
                 icon: image,
                 title: place.name,
+                info: place.vicinity,
                 position: place.geometry.location
-        });
+          });
 
 
           /* add click listner */
-    	marker[i].addListener('click', function() {
+    	  marker[i].addListener('click', function() {
 
               var directionsDisplay0 = new google.maps.DirectionsRenderer;
               directionsDisplay0.setMap(null);
@@ -163,15 +178,17 @@
               var origin_place_id=r_loc;
               var destination_place_id=d_loc;
 
-
               route(origin_place_id,destination_place_id,google.maps.TravelMode.DRIVING); // call function to show direction
 
         });
 
-        placesList.innerHTML += '<li>' + place.name + '</li>';
+        /* append list element in place bar */
+        placesList.innerHTML += '<li><div>' + place.name +'</div>'+'<div><font color=blue><b>Proximity</b>:&nbsp;' + place.vicinity + '</font></div>' +'</li>';
 
-    	google.maps.event.addListener(marker[i], 'click', function() {
-            infowindow.setContent(this.title);
+    	  google.maps.event.addListener(marker[i], 'click', function() {
+
+            var infoWindowContent = this.title;
+            infowindow.setContent(infoWindowContent);
             infowindow.open(map,this);
 
         });
@@ -183,7 +200,7 @@
   }
 
   /*  function for setting direction and show travel time  */
-	function route(origin_place_id, destination_place_id, travel_mode) {
+  function route(origin_place_id, destination_place_id, travel_mode) {
 
       if (!origin_place_id || !destination_place_id) {
         return;
@@ -200,15 +217,15 @@
 
             /* set direction on map */
             directionsDisplay.setDirections(response);
-    		    directionsDisplay.setMap(map);
+    	    directionsDisplay.setMap(map);
 
             /* add traffic layer */
             var trafficLayer = new google.maps.TrafficLayer();
             trafficLayer.setMap(map);
 
-            /* calculate estimated travel time */
+            /* calculate estimated travel time and and display them via alert */
             var point = response.routes[ 0 ].legs[ 0 ];
-            alert( 'Estimated travel time: ' + point.duration.text + ' (' + point.distance.text + ')');
+            alert( 'Estimated travel time of your selected Gas Station is : \n' + point.duration.text + ' (' + point.distance.text + ')');
 
           } else {
             window.alert('Directions request failed due to ' + status);
